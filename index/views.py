023d14926201731b771aaa94081f8d4cbe2d8379
Aayush Twayana <aayush.twayana@gmail.com>
2020-12-19 00:25:57 +0545

    completed check function

--- index/views.py
+++ index/views.py
@@ -66,16 +66,22 @@ def check(request, check_in, check_out, num_guests=1):
     if len(filtered_room) > 0:
         hotel_room_count = list()
         booked_room_count = list()
+        booked_room = list()
 
         for i in range((len(filtered_room))):
             hotel_room_count.append(RoomInstance.objects.filter(room = filtered_room[i]).count())
             booked_room_count.append(Book.objects.filter(room = filtered_room[i]).count())
+            booked_room.append(list(RoomInstance.objects.filter(room = filtered_room[i])))
 
         for i in range(len(hotel_room_count)):
             if booked_room_count[i] < hotel_room_count[i]:
                 available_room.append(filtered_room[i].room_name)
             else:
-                pass
+                for room in booked_room[i]:
+                    if check_in > room.check_out_date or check_out < room.check_in_date:
+                        available_room.append(filtered_room[i].room_name)
+                    else:
+                        pass
     else:
         request.session['error'] = f"No Rooms for {num_guests} guests.</br>Consider dividing the guests into different rooms."
     
