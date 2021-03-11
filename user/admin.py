3f215b080d7962813cfd96e0a9c62040aa298398
Aayush Twayana <aayush.twayana@gmail.com>
2021-03-11 12:46:01 +0545

    now admin can view room_no in user model in admin dashboard and added a reception account

--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from user.models import User
 
+from index.models import RoomInstance
+
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = (
@@ -8,6 +10,7 @@ class UserAdmin(admin.ModelAdmin):
         'u_email',
         'u_contact',
         'u_address',
+        'get_room_no',
         # 'u_room',     #if u want this, make a func that fetches this from the room_instance model
     )
 
@@ -17,3 +20,15 @@ class UserAdmin(admin.ModelAdmin):
     #     'u_contact',
     #     'u_address',
     # )
+
+    def get_room_no(self, obj):
+        try:
+            room = RoomInstance.objects.filter(user = obj)[0]   #above statement returns a queryset
+        except (IndexError):
+            room_no = None
+        else:
+            room_no = room.room_number
+        return room_no
+    
+    get_room_no.short_description = 'Room Number'
+    get_room_no.allow_tags = True
