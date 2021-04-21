a0301da81dea671f67e27b44369437217569dded
Aayush Twayana <aayush.twayana@gmail.com>
2021-04-21 08:18:58 +0545

    installed django-debug-toolbar and updated staticfiles

--- hotel/settings.py
+++ hotel/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '192.168.123.5']
 
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
+
 
 # Application definition
 
@@ -42,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -52,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'hotel.urls'
--- hotel/urls.py
+++ hotel/urls.py
@@ -13,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+import debug_toolbar
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -28,6 +31,7 @@ urlpatterns = [
     path('', include('index.urls')),
     path('book/', book_views.book, name='book'),
     path('admin/', admin.site.urls),
+    path('__debug__/', include(debug_toolbar.urls)),
 ] 
 
 if settings.DEBUG:
